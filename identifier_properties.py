72db6d7680e3f2fa5d9bbc3c198716691c4842ac
philsMINT <34623854+philsMINT@users.noreply.github.com>
2018-03-08 11:58:10 +0100

    Update identifier_properties.py
    Discart unneccessary code

--- identifier_properties.py
+++ identifier_properties.py
@@ -1,53 +1,12 @@
-from flask import Flask, render_template
-
 import pywikibot
-from collections import defaultdict
 from SPARQLWrapper import SPARQLWrapper, JSON
-import xmltodict
-import json, sys
+import json
 
-app = Flask(__name__)
+# example Wikidata formula QID and identifier search string
+# formula = "Q11432"
+# identifier = "R"
 
-# @app.route('/')
-# def my_form():
-#     return render_template("index.html")
-
-# formula Wikidata QID identifier search strings
-formula = "Q11432"
-identifier = "R"
-
-@app.route('/')
-def enter_formula():
-    return render_template("index.html")
-
-@app.route('/identifier_properties')
-def retrieve_identifier_properties():
-
-    try:
-        sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
-        sparql.setQuery("""#find identifier name
-        SELECT ?identifierLabel ?symbol WHERE {
-        wd:""" + formula + """ p:P527 ?statement. #p: points to statement node
-        ?statement ps:P527 ?identifier. #ps: property statement
-        ?statement pq:P2534 ?symbol. #ps: property qualifier
-        SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-        }""")
-        sparql.setReturnFormat(JSON)
-        results = sparql.query().convert()
-        tmp_results = []
-        for result in results["results"]["bindings"]:
-            tmp_results.append(result)
-
-        json_str = json.dumps(tmp_results)
-        resp = json.loads(json_str)
-        #return resp['identifierLabel']['value']
-        return json_str
-
-    except:
-        return "NA"
-
-@app.route('/identifier_name')
-def retrieve_identifier_name():
+def retrieve_identifier_name(formula, identifier):
 
     try:
         sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
@@ -68,13 +27,11 @@ def retrieve_identifier_name():
         json_str = json.dumps(tmp_results[0])
         resp = json.loads(json_str)
         return resp['identifierLabel']['value']
-        # return json_str
 
     except:
-        return "NA"
-
-@app.route('/identifier_value')
-def retrieve_identifier_value():
+        return ""
+    
+def retrieve_identifier_value(formula, identifier):
 
     try:
         sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
@@ -97,7 +54,4 @@ def retrieve_identifier_value():
         return resp['value']['value']
 
     except:
-        return "NA"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+        return ""
