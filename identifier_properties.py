4cdfa7a0187227026135b18d11ccada7f4f65102
Philipp <philipp.scharpf@uni-konstanz.de>
2018-03-25 14:56:35 +0200

    Add identifier retrieval via pywikibot

--- identifier_properties.py
+++ identifier_properties.py
@@ -2,21 +2,58 @@ import pywikibot
 from SPARQLWrapper import SPARQLWrapper, JSON
 import json
 
-# example Wikidata formula QID and identifier search string
-# formula = "Q11432"
-# identifier = "R"
+#---------
+#PYWIKIBOT
+#---------
 
-def retrieve_identifier_name(formula, identifier):
+def retrieve_identifiers(formulaName)
+
+    #retrieve Wikidata page item
+    identifiers = dict()
+    try:
+        site = pywikibot.Site("en", "wikipedia")
+        page = pywikibot.Page(site, formulaName)
+        item = pywikibot.ItemPage.fromPage(page)
+        #formulaQID = str(item).replace("[[wikidata:", '').replace("]]", '')
+        #formula_string = item.claims['P2534'][0].getTarget()
+
+        #retrieve identifiers
+        identifier_list = item.claims['P527']
+        identifier_symbol = ""
+        identifier_name = ""
+        identifier_value = ""
+        for identifier in identifier_list:
+
+            identifier_symbol = str(identifier.qualifiers['P2534'][0].target)
+            identifier_name = str(identifier.getTarget().text['labels']['en'])
+
+            identifiers[identifier_symbol] = {}
+            identifiers[identifier_symbol]['name'] = identifier_name
+            try:
+                identifier_value = str(identifier.getTarget().claims['P1181'][0].getTarget().amount)
+                identifiers[identifier_symbol]['value'] = identifier_value
+            except:
+                pass
+        return identifiers
+
+    except:
+        return dict()
+
+#------
+#SPARQL
+#------
+
+def retrieve_identifier_name(formulaQID, identifierSymbol):
 
     try:
         sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
         sparql.setQuery("""#find identifier name
         SELECT ?identifierLabel WHERE {
-        wd:""" + formula + """ p:P527 ?statement. #p: points to statement node
+        wd:""" + formulaQID + """ p:P527 ?statement. #p: points to statement node
         ?statement ps:P527 ?identifier. #ps: property statement
         ?statement pq:P2534 ?symbol. #ps: property qualifier
         SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-        FILTER(CONTAINS(STR(?symbol), '<mi>""" + identifier + """</mi>'))
+        FILTER(CONTAINS(STR(?symbol), '<mi>""" + identifierSymbol + """</mi>'))
         }""")
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
@@ -31,17 +68,17 @@ def retrieve_identifier_name(formula, identifier):
     except:
         return ""
     
-def retrieve_identifier_value(formula, identifier):
+def retrieve_identifier_value(formulaQID, identifierSymbol):
 
     try:
         sparql = SPARQLWrapper("https://query.wikidata.org/sparql")
         sparql.setQuery("""#find identifier value
         SELECT ?value WHERE {
-        wd:""" + formula + """ wdt:P527 ?identifier.
+        wd:""" + formulaQID + """ wdt:P527 ?identifier.
         ?identifier wdt:P416 ?symbol.
         ?identifier wdt:P1181 ?value.
         SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
-        FILTER(STR(?symbol) = '""" + identifier + """')
+        FILTER(STR(?symbol) = '""" + identifierSymbol + """')
         }""")
         sparql.setReturnFormat(JSON)
         results = sparql.query().convert()
